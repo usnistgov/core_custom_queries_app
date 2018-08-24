19f46e7b142b0000f92004b37a8ea471de3542fd
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2018-08-24 11:06:37 -0400

    fix: remove unused parameter
    Change-Id: Ic5319b6a1997413788e92d501ad82e927cdde28d

--- core_custom_queries_app/settings.py
+++ core_custom_queries_app/settings.py
@@ -5,4 +5,3 @@ if not settings.configured:
 
 REDIS_HOST = getattr(settings, 'REDIS_HOST', "127.0.0.1")
 REDIS_PORT = getattr(settings, 'REDIS_PORT', 6379)
-REDIS_URI = getattr(settings, 'REDIS_URI', "redis://%s:%d/0" % (REDIS_HOST, REDIS_PORT))
