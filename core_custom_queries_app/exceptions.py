faa4cf7baba1eb7f4fc599ac76f7b9a38dd3513a
Philippe Dessauw <philippe.dessauw@nist.gov>
2018-11-15 12:36:35 -0500

    fix: typo + string concat
    Change-Id: I11f0453fd1d77aa2ecc3b96b4a8459eb76ff5541

--- core_custom_queries_app/exceptions.py
+++ core_custom_queries_app/exceptions.py
@@ -1,10 +1,10 @@
-""" Exceptions
+""" Custom queries exceptions
 """
 
 
 class EmptyChoicesFromQuery(Exception):
-    def __init__(self, x_path='', schema=''):
-        if x_path != '':
-            self.message = "There is no choice for the user with the x_path: " + str(x_path)
+    def __init__(self, x_path="", schema=""):
+        if x_path != "":
+            self.message = "There is no choice for the user with the xpath %s" % str(x_path)
         else:
-            self.message = "There is not XMLData linked to the schema " + str(schema) + "."
+            self.message = "There is no XML data linked to the schema %s." % str(schema)
